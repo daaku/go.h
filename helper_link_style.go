8d506cc675173ba44c80745b327df4ab5ab0b140
Naitik Shah <n@daaku.org>
2012-04-26 17:34:46 -0700

    LinkStyle requires HREF

--- helper_link_style.go
+++ helper_link_style.go
@@ -1,10 +1,17 @@
 package h
 
+import (
+	"errors"
+)
+
 type LinkStyle struct {
 	HREF string
 }
 
 func (l *LinkStyle) HTML() (HTML, error) {
+	if l.HREF == "" {
+		return nil, errors.New("Missing HREF in LinkStyle.")
+	}
 	return &Link{
 		Type: "text/css",
 		Rel:  "stylesheet",
