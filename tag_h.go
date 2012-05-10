4f04c605094a76bfc3d0d667ed22ee2982133688
Naitik Shah <n@daaku.org>
2012-05-09 17:44:31 -0700

    h2

--- tag_h.go
+++ tag_h.go
@@ -11,3 +11,15 @@ type H1 struct {
 func (h *H1) HTML() (HTML, error) {
 	return &ReflectNode{Tag: "h1", Node: h}, nil
 }
+
+type H2 struct {
+	ID    string `h:"attr"`
+	Class string `h:"attr"`
+	Style string `h:"attr"`
+	Title string `h:"attr"`
+	Inner HTML   `h:"inner"`
+}
+
+func (h *H2) HTML() (HTML, error) {
+	return &ReflectNode{Tag: "h2", Node: h}, nil
+}
