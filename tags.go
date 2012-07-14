d9efa5707fd88c2a2c131bc37d8a43a49ef6f578
Naitik Shah <n@daaku.org>
2012-07-14 02:09:08 -0700

    placeholder for input

--- tags.go
+++ tags.go
@@ -141,13 +141,14 @@ func (t *Img) HTML() (HTML, error) {
 }
 
 type Input struct {
-	ID    string `h:"attr"`
-	Class string `h:"attr"`
-	Name  string `h:"attr"`
-	Style string `h:"attr"`
-	Type  string `h:"attr"`
-	Value string `h:"attr"`
-	Inner HTML   `h:"inner"`
+	ID          string `h:"attr"`
+	Class       string `h:"attr"`
+	Name        string `h:"attr"`
+	Style       string `h:"attr"`
+	Type        string `h:"attr"`
+	Value       string `h:"attr"`
+	Placeholder string `h:"attr"`
+	Inner       HTML   `h:"inner"`
 }
 
 func (i *Input) HTML() (HTML, error) {
