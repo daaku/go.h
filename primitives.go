c11bc98d006114f4a9d0b7082cdb1c8328855324
Naitik Shah <n@daaku.org>
2015-09-18 22:44:38 -0700

    combine primitives into 1 file

--- primitives.go
+++ primitives.go
@@ -2,12 +2,72 @@ package h
 
 import (
 	"fmt"
+	"html"
 	"io"
 	"reflect"
 	"strings"
 
 	"golang.org/x/net/context"
 )
+
+var _ HTML = (*Frag)(nil)
+var _ Primitive = (*Frag)(nil)
+
+type Frag []HTML
+
+func (f *Frag) HTML(ctx context.Context) (HTML, error) {
+	return f, fmt.Errorf("Frag.HTML called for %s", f)
+}
+
+func (f *Frag) Append(h HTML) {
+	*f = append(*f, h)
+}
+
+func (f *Frag) Write(ctx context.Context, w io.Writer) (int, error) {
+	written := 0
+	for _, e := range *f {
+		i, err := Write(ctx, w, e)
+		written += i
+		if err != nil {
+			return written, err
+		}
+	}
+	return written, nil
+}
+
+var _ HTML = String("")
+
+type String string
+
+func (s String) HTML(ctx context.Context) (HTML, error) {
+	return Unsafe(html.EscapeString(string(s))), nil
+}
+
+var _ HTML = Unsafe("")
+var _ Primitive = Unsafe("")
+
+type Unsafe string
+
+func (u Unsafe) HTML(ctx context.Context) (HTML, error) {
+	return u, fmt.Errorf("Unsafe.HTML called for %s", u)
+}
+
+func (u Unsafe) Write(ctx context.Context, w io.Writer) (int, error) {
+	return fmt.Fprint(w, u)
+}
+
+var _ HTML = UnsafeBytes(nil)
+var _ Primitive = UnsafeBytes(nil)
+
+type UnsafeBytes []byte
+
+func (u UnsafeBytes) HTML(ctx context.Context) (HTML, error) {
+	return u, fmt.Errorf("UnsafeBytes.HTML called for %s", u)
+}
+
+func (u UnsafeBytes) Write(ctx context.Context, w io.Writer) (int, error) {
+	return fmt.Fprintf(w, "%s", u)
+}
 
 var _ HTML = (*ReflectNode)(nil)
 var _ Primitive = (*ReflectNode)(nil)
