103650c1e785ef047fd28ea02ad583b2f3fd89e7
Naitik Shah <n@daaku.org>
2015-09-18 22:51:21 -0700

    combine document.go into tags.go

--- tags.go
+++ tags.go
@@ -2,6 +2,44 @@ package h
 
 import "golang.org/x/net/context"
 
+type XMLNS map[string]string
+
+func (ns XMLNS) Attributes() Attributes {
+	attrs := Attributes{}
+	for key, val := range ns {
+		attrs["xmlns:"+key] = val
+	}
+	return attrs
+}
+
+var _ HTML = (*Document)(nil)
+
+type Document struct {
+	XMLNS XMLNS
+	Inner HTML
+	ID    string
+	Lang  string
+}
+
+func (d *Document) HTML(ctx context.Context) (HTML, error) {
+	attrs := d.XMLNS.Attributes()
+	if d.ID != "" {
+		attrs["id"] = d.ID
+	}
+	if d.Lang != "" {
+		attrs["lang"] = "en"
+	}
+
+	return &Frag{
+		Unsafe("<!doctype html>"),
+		&Node{
+			Tag:        "html",
+			Attributes: attrs,
+			Inner:      d.Inner,
+		},
+	}, nil
+}
+
 var _ HTML = (*A)(nil)
 
 type A struct {
--- tags_test.go
+++ tags_test.go
@@ -1,9 +1,35 @@
 package h_test
 
 import (
-	"github.com/daaku/go.h"
 	"testing"
+
+	"github.com/daaku/go.h"
 )
+
+func TestEmptyDocument(t *testing.T) {
+	t.Parallel()
+	doc := &h.Document{}
+	assertRender(t, doc, `<!doctype html><html></html>`)
+}
+
+func TestFacebookXMLNS(t *testing.T) {
+	t.Parallel()
+	doc := &h.Document{
+		XMLNS: h.XMLNS{
+			"fb": "http://ogp.me/ns/fb#",
+		},
+	}
+	assertRender(t, doc,
+		`<!doctype html><html xmlns:fb="http://ogp.me/ns/fb#"></html>`)
+}
+
+func TestDocumentLangAttr(t *testing.T) {
+	t.Parallel()
+	doc := &h.Document{
+		Lang: "en",
+	}
+	assertRender(t, doc, `<!doctype html><html lang="en"></html>`)
+}
 
 func TestEmptyBody(t *testing.T) {
 	t.Parallel()
